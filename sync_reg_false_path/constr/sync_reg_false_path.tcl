64f0ade8b30a766b97d24f556fcc22d4a9edbc3d
Michał Kruszewski <mkru@protonmail.com>
2019-07-25 09:13:06 +0200

    Fix file types for sync_reg_false_path constraints.

--- sync_reg_false_path/constr/sync_reg_false_path.tcl
+++ sync_reg_false_path/constr/sync_reg_false_path.tcl
@@ -1,4 +1,4 @@
-if {[string match "Vivado*" [version]]}
+if {[string match "Vivado*" [version]]} {
     set_property SCOPED_TO_REF sync_reg_false_path [get_files sync_reg_false_path.xdc]
 } else {
     error "sync_reg_false_path entity misses constraint file for your EDA tool"

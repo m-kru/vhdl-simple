48772cd3d5fed5a9eae60274125f690f384ea8ec
Michał Kruszewski <mkru@protonmail.com>
2019-09-17 07:34:26 +0200

    Fix error message in reset_synchronizer.tcl.

--- reset_synchronizer/constr/reset_synchronizer.tcl
+++ reset_synchronizer/constr/reset_synchronizer.tcl
@@ -1,5 +1,5 @@
 if {[string match "Vivado*" [version]]} {
     set_property SCOPED_TO_REF reset_synchronizer [get_files reset_synchronizer.xdc]
 } else {
-    error "sync_reg_false_path entity misses constraint file for your EDA tool"
+    error "reset_synchronizer entity misses constraint file for your EDA tool"
 }

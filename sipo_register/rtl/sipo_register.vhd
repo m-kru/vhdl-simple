1ab1b3c73221cadf6cedf5c3eecb881c05576dcb
Michał Kruszewski <mkru@protonmail.com>
2020-03-31 14:49:56 +0200

    Add sipo_register entity.

---
+++ sipo_register/rtl/sipo_register.vhd
@@ -0,0 +1,98 @@
+--------------------------------------------------------------------------------
+-- vhdl_simple library
+-- https://github.com/m-kru/vhdl_simple
+--------------------------------------------------------------------------------
+--
+-- Entity: Serial-In Parallel-Out (SIPO) register.
+--
+-- Description:
+--  SIPO register which can be configured to work with or without output
+--  register. When G_REGISTER_OUTPUTS is set to false, the strobe_i input can
+--  be left unconnected.
+--
+--------------------------------------------------------------------------------
+-- Copyright (c) 2019 Michal Kruszewski
+--------------------------------------------------------------------------------
+-- MIT License
+--------------------------------------------------------------------------------
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+--------------------------------------------------------------------------------
+
+library ieee;
+  use ieee.std_logic_1164.all;
+
+entity sipo_register is
+  generic (
+    G_WIDTH            : positive  := 8;
+    G_INIT_VALUE       : std_logic := 'U';
+    G_RESET_VALUE      : std_logic := '0';
+    G_REGISTER_OUTPUTS : boolean   := true
+  );
+  port (
+    clk_i    : in   std_logic;
+    rst_i    : in   std_logic;
+    d_i      : in   std_logic;
+    d_o      : out  std_logic;
+    q_o      : out  std_logic_vector(G_WIDTH - 1 downto 0); -- Parallel output
+    -- Strobe is used only if G_REGISTER_OUTPUTS is set to true.
+    strobe_i : in   std_logic := 'U'
+  );
+
+end entity sipo_register;
+
+architecture rtl of sipo_register is
+
+  signal q_internal : std_logic_vector(G_WIDTH - 1 downto 0) := (others => G_INIT_VALUE);
+  signal q_output   : std_logic_vector(G_WIDTH - 1 downto 0) := (others => G_INIT_VALUE);
+
+begin
+
+  d_o <= q_internal(G_WIDTH - 1);
+
+  shift : process(clk_i)
+  begin
+    if rising_edge(clk_i) then
+      if rst_i = '1' then
+        q_internal <= (others => G_RESET_VALUE);
+      else
+        for i in 0 to G_WIDTH - 1 loop
+          if i = 0 then
+            q_internal(0) <= d_i;
+          else
+            q_internal(i) <= q_internal(i - 1);
+          end if;
+        end loop;
+      end if;
+    end if;
+  end process shift;
+
+  register_outputs : if G_REGISTER_OUTPUTS generate
+  
+    q_o <= q_output;
+
+    process (clk_i) is
+    begin
+  
+      if rising_edge(clk_i) then
+        if rst_i = '1' then
+          q_output <= (others => G_RESET_VALUE);
+        elsif strobe_i = '1' then
+          q_output <= q_internal;
+        end if; 
+      end if;
+
+    end process;
+  
+  else generate
+
+    q_o <= q_internal;
+
+  end generate register_outputs;
+
+end architecture rtl;
---
+++ sipo_register/tb/tb_no_output_registers.vhd
@@ -0,0 +1,66 @@
+library ieee;
+  use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
+
+entity tb_no_output_registers is
+end entity;
+
+architecture tb_arch of tb_no_output_registers is
+
+  constant CLK_PERIOD : time := 10 ns;
+  signal clk   : std_logic := '0';
+  signal reset : std_logic := '0';
+
+  signal din    : std_logic := '0';
+  signal dout   : std_logic;
+  signal q      : std_logic_vector(3 downto 0);
+
+  constant C_TEST_SEQUENCE : std_logic_vector(3 downto 0) := "0110";
+begin
+
+  clk <= not clk after CLK_PERIOD/2;
+
+  sipo_register_0 : entity work.sipo_register
+  generic map (
+    G_WIDTH            => 4,
+    G_REGISTER_OUTPUTS => false
+  )
+  port map (
+    clk_i => clk,
+    rst_i => reset,
+    d_i   => din,
+    d_o   => dout,
+    q_o   => q
+  );
+
+  stimuli_generator: process
+  begin
+    wait for 4 * CLK_PERIOD;
+
+    for i in 3 downto 0 loop
+      din <= C_TEST_SEQUENCE(i);
+      wait for  CLK_PERIOD;
+    end loop;
+
+    reset <= '1';
+    wait for  CLK_PERIOD;
+    reset <= '0';
+
+    wait for 4 * CLK_PERIOD;
+    report "End of testbench. All tests passed." severity note;
+    std.env.finish;
+  end process;
+
+  response_checker: process
+  begin
+    wait for 4 * CLK_PERIOD;
+    assert q = "0000" report "q should be all 0" severity failure;
+
+    wait for 4 * CLK_PERIOD;
+    assert q = C_TEST_SEQUENCE report "q should equal to C_TEST_SEQUENCE" severity failure;
+
+    wait for CLK_PERIOD;
+    assert q = "0000" report "q should equal all 0 after reset" severity failure;
+  end process;
+
+end;

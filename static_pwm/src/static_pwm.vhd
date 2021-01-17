db28cc2761e0b393750b7ceb0f09672251ff81a7
Michał Kruszewski <mkru@protonmail.com>
2021-01-17 13:52:13 +0100

    static_pwm: add G_START_ON_RESET generic.

--- static_pwm/src/static_pwm.vhd
+++ static_pwm/src/static_pwm.vhd
@@ -14,11 +14,18 @@ library ieee;
 -- Useful when G_PERIOD and G_DUTY are relatively small numbers.
 --
 -- G_DISABLED_VALUE is the value set to the out_o when module is disabled.
+--
+-- G_START_ON_RESET determines whether counting should start on reset
+-- (G_START_ON_RESET = true) or after the rst_i is deasserted
+-- (G_START_ON_RESET = false). G_RESET_VALUE is used only if
+-- G_START_ON_RESET = false.
 entity static_pwm is
    generic (
       G_PERIOD        : positive;
       G_DUTY          : positive;
-      G_DISABLED_VALUE : std_logic := '0'
+      G_DISABLED_VALUE : std_logic := '0';
+      G_START_ON_RESET : boolean   := true;
+      G_RESET_VALUE    : std_logic := '0'
    );
    port (
       clk_i    : in  std_logic;
@@ -35,30 +42,64 @@ end entity;
 architecture behavioral of static_pwm is
 begin
 
-   process (clk_i) is
-      variable counter : natural range 0 to G_PERIOD - 1;
-   begin
-      if rising_edge(clk_i) then
-         if rst_i = '1' then
-            counter := 0;
-         else
-            if counter = G_PERIOD - 1  then
+   reset_policy : if G_START_ON_RESET = true generate
+
+      process (clk_i) is
+         variable counter : natural range 0 to G_PERIOD - 1;
+      begin
+         if rising_edge(clk_i) then
+            if rst_i = '1' then
                counter := 0;
             else
-               counter := counter + 1;
+               if counter = G_PERIOD - 1  then
+                  counter := 0;
+               else
+                  counter := counter + 1;
+               end if;
+            end if;
+
+            out_o <= '1';
+
+            if counter >= G_DUTY then
+               out_o <= '0';
+            end if;
+
+            if enable_i = '0' then
+               out_o <= G_DISABLED_VALUE;
             end if;
          end if;
+      end process;
 
-         out_o <= '1';
+   else generate
 
-         if counter >= G_DUTY then
-            out_o <= '0';
+      process (clk_i) is
+         variable counter : natural range 0 to G_PERIOD;
+      begin
+         if rising_edge(clk_i) then
+            if rst_i = '1' then
+               counter := 0;
+            else
+               if counter = G_PERIOD then
+                  counter := 1;
+               else
+                  counter := counter + 1;
+               end if;
+            end if;
+
+            out_o <= '1';
+
+            if counter = 0 then
+               out_o <= G_RESET_VALUE;
+            elsif counter > G_DUTY then
+               out_o <= '0';
+            end if;
+
+            if enable_i = '0' then
+               out_o <= G_DISABLED_VALUE;
+            end if;
          end if;
+      end process;
 
-         if enable_i = '0' then
-            out_o <= G_DISABLED_VALUE;
-         end if;
-      end if;
-   end process;
+   end generate;
 
 end architecture;
---
+++ static_pwm/tb/tb_start_after_reset.vhd
@@ -0,0 +1,85 @@
+library ieee;
+   use ieee.std_logic_1164.all;
+
+library simple;
+
+entity tb_start_after_reset is
+end entity;
+
+architecture test of tb_start_after_reset is
+   constant C_PERIOD : positive := 10;
+   constant C_DUTY   : positive := 3;
+
+   constant C_CLK_PERIOD : time := 10 ns;
+   signal clk : std_logic := '0';
+
+   signal rst : std_logic := '0';
+   signal enable : std_logic := '1';
+
+   signal s_out : std_logic;
+
+   signal ones_count : natural := 0;
+begin
+
+   clk <= not clk after C_CLK_PERIOD / 2;
+
+
+   DUT : entity simple.static_pwm
+   generic map (
+      G_PERIOD         => C_PERIOD,
+      G_DUTY           => C_DUTY,
+      G_START_ON_RESET => false
+   )
+   port map (
+      clk_i    => clk,
+      rst_i    => rst,
+      enable_i => enable,
+      out_o    => s_out
+   );
+
+
+   main : process
+   begin
+      wait for 25 * C_CLK_PERIOD;
+
+      rst <= '1';
+      wait for C_CLK_PERIOD;
+      rst <= '0';
+
+      wait for C_PERIOD * C_CLK_PERIOD;
+      report "End of testbench. All tests passed." severity note;
+      std.env.finish;
+   end process;
+
+
+   count_ones : process (clk)
+   begin
+      if rising_edge(clk) then
+         if s_out = '1' then
+            ones_count <= ones_count + 1;
+         end if;
+      end if;
+   end process;
+
+
+   checker : process
+   begin
+      wait for C_PERIOD * C_CLK_PERIOD;
+      assert ones_count = 3 report "After first period number of ones should equal 3" severity failure;
+      wait for C_PERIOD * C_CLK_PERIOD;
+      assert ones_count = 6 report "After second period number of ones should equal 6" severity failure;
+      wait;
+   end process;
+
+
+   checker_reset : process
+      variable old_ones_count : natural := 0;
+   begin
+      wait until rst = '1';
+      old_ones_count := ones_count;
+      wait until rst = '0';
+      assert ones_count = old_ones_count report "Wrong count of ones after reset" severity failure;
+   end process;
+
+
+end architecture;

7f5deb11617ac12c966eb496a84c27ed5b292ab4
Michał Kruszewski <mkru@protonmail.com>
2021-02-17 13:59:08 +0100

    write_enable_i -> wr_en_i

--- accumulative_bit_array/rtl/accumulative_bit_array.vhd
+++ accumulative_bit_array/rtl/accumulative_bit_array.vhd
@@ -31,11 +31,11 @@ entity accumulative_bit_array is
     G_WIDTH : positive := 32
   );
   port (
-    clk_i          : in    std_logic;
-    rst_i          : in    std_logic;
-    write_enable_i : in    std_logic := '1';
-    d_i            : in    std_logic_vector(G_WIDTH - 1 downto 0);
-    q_o            : out   std_logic_vector(G_WIDTH - 1 downto 0)
+    clk_i   : in    std_logic;
+    rst_i   : in    std_logic;
+    wr_en_i : in    std_logic := '1';
+    d_i     : in    std_logic_vector(G_WIDTH - 1 downto 0);
+    q_o     : out   std_logic_vector(G_WIDTH - 1 downto 0)
   );
 end entity accumulative_bit_array;
 
@@ -53,7 +53,7 @@ begin
     if rising_edge(clk_i) then
       if rst_i = '1' then
         q_r   <= (others => '0');
-      elsif write_enable_i = '1' then
+      elsif wr_en_i = '1' then
         for_each_bit : for i in 0 to G_WIDTH - 1 loop
           if q_r(i) = '0' then
             q_r(i) <= d_i(i);

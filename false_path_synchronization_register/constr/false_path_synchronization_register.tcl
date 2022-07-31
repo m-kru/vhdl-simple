e474bd90b453e87ceaf2a0af4f3d65e61eb9e62a
Michał Kruszewski <mkru@protonmail.com>
2022-07-31 14:58:06 +0200

    Remove false_path_synchronization_register.

--- false_path_synchronization_register/constr/false_path_synchronization_register.tcl
+++
@@ -1,5 +0,0 @@
-if {[string match "Vivado*" [version]]} {
-    set_property SCOPED_TO_REF False_Path_Synchronization_Register [get_files False_Path_Synchronization_Register.xdc]
-} else {
-    error "False_Path_Synchronization_Register entity misses constraint file for your EDA tool"
-}
--- false_path_synchronization_register/constr/false_path_synchronization_register.xdc
+++
@@ -1,1 +0,0 @@
-set_false_path -to [get_pins s_0_reg[*]/D]
